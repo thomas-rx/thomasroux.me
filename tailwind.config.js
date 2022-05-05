b6ead8947509448e9f0423f50e51b1f08720f158
Thomas ROUX <t.roux@pyres.com>
2022-05-05 16:32:47 +0200

    Revert ":art: Improve configuration format"
    This reverts commit b6f35383482682aeb5457ca771bad79e4c3a3c17.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,11 @@
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
-        colors: {
-            'blue': '#1fb6ff',
-            'pink': '#ff49db',
-            'orange': '#ff7849',
-            'green': '#13ce66',
-            'gray-dark': '#273444',
-            'gray': '#8492a6',
-            'gray-light': '#d3dce6',
-        },
         extend: {
             fontFamily: {
                 ubuntu: ['Ubuntu', 'sans-serif'],
             },
         },
-    }
+    },
+    plugins: [],
 }
