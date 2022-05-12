4676257042496090dc99d4951c2f096dfadcee4b
Thomas ROUX <t.roux@pyres.com>
2022-05-12 16:04:47 +0200

    :lipstick: Loading improved

--- src/components/landing/SectionTitle.jsx
+++ src/components/landing/SectionTitle.jsx
@@ -8,43 +8,41 @@ import { GradienText } from '../common/Text/GradientText';
 export function SectionTitle(props) {
   const { trackEvent } = useMatomo();
   return (
-    <div className='h-screen section'>
-      <div className='grid content-center font-sans'>
-        <span className='justify-center text-center font-black'>
-          <h1 className='text-5xl md:text-7xl'>
-            <span>
-              <GradienText text={props.title} />
-            </span>
-          </h1>
+    <div className='grid content-center h-screen font-sans section'>
+      <span className='justify-center font-black text-center'>
+        <h1 className='text-5xl md:text-7xl'>
+          <span>
+            <GradienText text={props.title} />
+          </span>
+        </h1>
 
-          {props.description !== undefined && (
-            <h2 className='m-[10%] md:m-[2%] text-xl'>
-              <GradienText text={props.description} />
-            </h2>
-          )}
+        {props.description !== undefined && (
+          <h2 className='m-[10%] md:m-[2%] text-xl'>
+            <GradienText text={props.description} />
+          </h2>
+        )}
 
-          {props.btnText !== undefined && (
-            <div className='flex items-center justify-center w-screen animate-bounce'>
-              <button
-                onClick={(e) => {
-                  props.fullpageApi.moveSectionDown();
-                  trackEvent({
-                    category: 'Click',
-                    action: e.target.innerText,
-                  });
-                }}
-                className='flex justify-center px-5 py-2 mx-2 mt-10 text-xl font-black text-green-800 transition ease-in-out border-2 rounded-full border-lime-500 hover:bg-lime-500 hover:text-white hover:-translate-y-1 dark:text-lime-200'>
-                <div>
-                  <span>{props.btnText}</span>
-                </div>
-                <a href='#project' className='ml-2'>
-                  <FontAwesomeIcon icon={faAngleDown} />
-                </a>
-              </button>
-            </div>
-          )}
-        </span>
-      </div>
+        {props.btnText !== undefined && (
+          <div className='flex items-center justify-center w-screen animate-bounce'>
+            <button
+              onClick={(e) => {
+                props.fullpageApi.moveSectionDown();
+                trackEvent({
+                  category: 'Click',
+                  action: e.target.innerText,
+                });
+              }}
+              className='flex justify-center px-5 py-2 mx-2 mt-10 text-xl font-black text-green-800 transition ease-in-out border-2 rounded-full border-lime-500 hover:bg-lime-500 hover:text-white hover:-translate-y-1 dark:text-lime-200'>
+              <div>
+                <span>{props.btnText}</span>
+              </div>
+              <a href='#project' className='ml-2'>
+                <FontAwesomeIcon icon={faAngleDown} className='w-[10px]' />
+              </a>
+            </button>
+          </div>
+        )}
+      </span>
     </div>
   );
 }
