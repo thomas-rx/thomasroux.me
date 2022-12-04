50c043d3a90eb793107084c520bc5060fae905c2
xrths <contact@xrths.fr>
2022-12-04 20:00:33 +0100

    :art: Improved UI

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,47 +1,47 @@
-const config = require("./src/data/config");
+const config = require('./src/data/config');
 
 module.exports = {
-    siteMetadata: {
-        title: config.defaultTitle,
-        description: config.description,
-        keywords: config.keywords,
-        username: config.username,
-        email: config.email,
+  siteMetadata: {
+    title: config.defaultTitle,
+    description: config.description,
+    keywords: config.keywords,
+    username: config.username,
+    email: config.email,
 
-        canonicalUrl: config.url,
-        siteUrl: config.url,
+    canonicalUrl: config.url,
+    siteUrl: config.url,
 
-        wordlyUrl: config.projectLinks.wordly,
-        linkedinUrl: config.socialLinks.linkedin,
-        githubUrl: config.socialLinks.github,
-        twitterUrl: config.socialLinks.twitter,
-        maltUrl: config.socialLinks.malt,
+    wordlyUrl: config.projectLinks.wordly,
+    linkedinUrl: config.socialLinks.linkedin,
+    githubUrl: config.socialLinks.github,
+    twitterUrl: config.socialLinks.twitter,
+    maltUrl: config.socialLinks.malt,
 
-        matomo: {
-            siteId: config.matomo.siteId,
-            urlBase: config.matomo.urlBase,
-            trackerUrl: config.matomo.trackerUrl,
-            srcUrl: config.matomo.srcUrl,
-        },
+    matomo: {
+      siteId: config.matomo.siteId,
+      urlBase: config.matomo.urlBase,
+      trackerUrl: config.matomo.trackerUrl,
+      srcUrl: config.matomo.srcUrl,
     },
-    plugins: [
-        `gatsby-plugin-react-helmet`,
-        `gatsby-plugin-postcss`,
-        {
-            resolve: "gatsby-plugin-sitemap",
-            exclude: ["/404*", "/*/404*"],
-        },
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: config.title,
-                short_name: config.shortName,
-                start_url: `/`,
-                background_color: `#fff`,
-                theme_color: `#fff`,
-                display: `standalone`,
-                icon: config.logo,
-            },
-        },
-    ],
-}
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-postcss`,
+    {
+      resolve: 'gatsby-plugin-sitemap',
+      exclude: ['/404*', '/*/404*'],
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: config.title,
+        short_name: config.shortName,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#fff`,
+        display: `standalone`,
+        icon: config.logo,
+      },
+    },
+  ],
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
+const colors = require('tailwindcss/colors');
 module.exports = {
-    content: ['./src/**/*.{js,jsx,ts,tsx}'],
-    theme: {
-        extend: {
-            fontFamily: {
-                'Ubuntu': ['Ubuntu', 'sans-serif'],
-            },
-        },
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        Ubuntu: ['Ubuntu', 'sans-serif'],
+        extra: ['Source Sans Pro', 'sans-serif'],
+      },
+      colors: {
+        primaryLight: colors.teal[700],
+        primaryDark: colors.teal[600],
+        secondaryLight: colors.teal[900],
+        secondaryDark: colors.emerald[200],
+        neutralLight: colors.gray[100],
+        neutralDark: colors.gray[900],
+      },
     },
-}
+  },
+};
