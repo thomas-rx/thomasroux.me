9bf33485258a7904a5170b62b4fc4cf1131d7dbd
xrths <contact@xrths.fr>
2022-12-10 03:03:29 +0100

    :art: Added support of SVG files

--- src/components/containers/index/myIntroduction.jsx
+++ src/components/containers/index/myIntroduction.jsx
@@ -1,49 +1,47 @@
 import React from "react";
-import { Personal } from "../../sections/personal";
-import PresentationImage from "/src/images/commons/presentation.png";
+import {Personal} from "../../sections/personal";
 
 export function MyIntroduction() {
-  return (
-    <Personal
-      title="Présentation"
-      image={PresentationImage}
-      text={
-        <p className="leading-relaxed">
+    return (
+        <Personal
+            title="Présentation"
+            text={
+                <p className="leading-relaxed">
           <span>
             {" "}
-            Étudiant en dernière année de Licence Informatique à{" "}
-            <a
-              href="https://www.univ-amu.fr/"
-              target="_blank"
-              rel="noreferrer"
-              className="text-primaryLight dark:text-primaryDark">
+              Étudiant en dernière année de Licence Informatique à{" "}
+              <a
+                  href="https://www.univ-amu.fr/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="text-primaryLight dark:text-primaryDark">
               Aix-Marseille
             </a>
             , je suis actuellement à la recherche d'un stage de fin d'études en
             génie logiciel.
           </span>
-          <br />
-          <br />
-          <span>
+                    <br/>
+                    <br/>
+                    <span>
             Si vous souhaitez en savoir plus sur mon parcours, n'hésitez pas à
             consulter mon{" "}
-            <a
-              href="https://www.linkedin.com/in/xrths/"
-              target="_blank"
-              rel="noreferrer"
-              className="text-primaryLight dark:text-primaryDark">
+                        <a
+                            href="https://www.linkedin.com/in/xrths/"
+                            target="_blank"
+                            rel="noreferrer"
+                            className="text-primaryLight dark:text-primaryDark">
               LinkedIn
             </a>{" "}
-            ou à me contacter par{" "}
-            <a
-              href="mailto:contact@xrths.fr"
-              className="text-primaryLight dark:text-primaryDark">
+                        ou à me contacter par{" "}
+                        <a
+                            href="mailto:contact@xrths.fr"
+                            className="text-primaryLight dark:text-primaryDark">
               mail
             </a>
             .
           </span>
-        </p>
-      }
-    />
-  );
+                </p>
+            }
+        />
+    );
 }
